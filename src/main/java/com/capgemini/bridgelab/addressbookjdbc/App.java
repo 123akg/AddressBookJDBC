a18f3046d80807ad0caeb4d4c4dd695c8a6fcffb
123akg <abhijeetkumargiri7070@gmail.com>
2020-11-04 23:48:32 +0530

    Added: Master branch

---
+++ src/main/java/com/capgemini/bridgelab/addressbookjdbc/App.java
@@ -0,0 +1,13 @@
+package com.capgemini.bridgelab.addressbookjdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Address Book Program!!!" );
+    }
+}
